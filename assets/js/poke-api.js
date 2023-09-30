b78609b85418acfc6e6f59af24720cd5d8dd6b5c
Mateus <mateus.alves8@fatec.sp.gov.br>
2023-09-30 13:14:05 -0300

    1

---
+++ assets/js/poke-api.js
@@ -0,0 +1,42 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    // Armazena os detalhes do Pokémon
+    pokemon.details = {
+        height: pokeDetail.height,
+        weight: pokeDetail.weight,
+        abilities: pokeDetail.abilities.map(ability => ability.ability.name),
+        baseExperience: pokeDetail.base_experience
+    };
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
